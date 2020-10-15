8fc9c6eb6c07f0f8e1ffdc2ba63d8842b048e840
Nitin Das <37865475+nitinda@users.noreply.github.com>
2020-10-15 13:47:06 +0000

    Added additional test data

--- api/auto/data.go
+++ api/auto/data.go
@@ -6,68 +6,83 @@ import (
 
 var users = []models.User{
 	models.User{
-		Username: "hybris",
-		Email:    "hybris@change.com",
+		Username: "sale",
+		Email:    "sale@changeapi.com",
 		Password: "123453433",
 	},
-	// models.User{
-	// 	Username: "Rowan",
-	// 	Email:    "Rowan@change.com",
-	// 	Password: "123453433",
-	// },
+	models.User{
+		Username: "inventory",
+		Email:    "inventory@changeapi.com",
+		Password: "97878789778",
+	},
+	models.User{
+		Username: "postman",
+		Email:    "postman@changeapi.com",
+		Password: "4545454544",
+	},
 }
 
 // configLogs sample data
 var configLogs = []models.ConfigLog{
 	models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
 		Service: "hybris",
-		Team:    "hybris-dev",
+		Team:    "sale",
 		Message: "This is test config entry",
 		UserID:  1,
 	}, models.ConfigLog{
-		Service: "hybris",
-		Team:    "hybris-dev",
+		Service: "inventoryData",
+		Team:    "inventory-dev",
 		Message: "This is test config entry",
-		UserID:  1,
+		UserID:  2,
+	}, models.ConfigLog{
+		Service: "inventory",
+		Team:    "inventory-dev",
+		Message: "This is test config entry",
+		UserID:  2,
+	}, models.ConfigLog{
+		Service: "inventory",
+		Team:    "inventory-dev",
+		Message: "This is test config entry",
+		UserID:  2,
 	},
 }
